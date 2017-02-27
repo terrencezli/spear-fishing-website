6199b649177df32391a8303a48bf77c081f40f58
= <terrencezli@hotmail.com>
2017-02-27 13:49:50 -0800

    other scripts

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-'use strict';
+#!/usr/bin/env node
 
-var express = require('express');
+var express = require("express"),
+  app = express(),
+  bodyParser = require('body-parser'),
+  errorHandler = require('errorhandler'),
+  methodOverride = require('method-override'),
+  hostname = process.env.HOSTNAME || 'localhost',
+  port = parseInt(process.env.PORT, 10) || 4567,
+  publicDir = process.argv[2] || __dirname + '/public',
+  path = require('path');
 
-var server = express();
-server.use(function(req, res, next) {
-  console.log(req.url);
-  next();
+app.get("/", function (req, res) {
+  res.sendFile(path.join(publicDir, "/index.html"));
 });
 
-var port = 8080;
-server.listen(port, function() {
-  console.log('server listening on port ' + port);
-});
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
+app.listen(port, hostname);
